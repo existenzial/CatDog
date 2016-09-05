2ba24314250a8f6a4a966e564c31ecacfccee76e
Jazz Lyles <ripley36706@gmail.com>
2016-09-05 12:41:16 -0700

    relocated model tests out of controller tests

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -4,6 +4,7 @@ class PeopleControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  
   test "should not save a person without a height" do
 
   end
@@ -17,42 +18,6 @@ class PeopleControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should not save a person without a prediction" do
-
-  end
-  # Height
-  test "height should be an integer" do
-
-  end
-
-  test "height should be greater than 36 inches" do
-
-  end
-
-  test "height should be less than 96 inches" do
-
-  end
-  # Weight
-  test "weight should be an integer" do
-
-  end
-
-  test "weight should be greater than 50 lbs" do
-
-  end
-
-  test "weight should be less than 350 lbs" do
-
-  end
-  # BMI
-  test "bmi should be a float" do
-
-  end
-
-  test "bmi should be greater than 4" do
-
-  end
-
-  test "bmi should be less than 40" do
 
   end
 
--- test/models/person_test.rb
+++ test/models/person_test.rb
@@ -4,4 +4,42 @@ class PersonTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  # Height
+  test "height should be an integer" do
+
+  end
+
+  test "height should be greater than 36 inches" do
+
+  end
+
+  test "height should be less than 96 inches" do
+
+  end
+  # Weight
+  test "weight should be an integer" do
+
+  end
+
+  test "weight should be greater than 50 lbs" do
+
+  end
+
+  test "weight should be less than 350 lbs" do
+
+  end
+  # BMI
+  test "bmi should be a float" do
+
+  end
+
+  test "bmi should be greater than 4" do
+
+  end
+
+  test "bmi should be less than 40" do
+
+  end
+
 end
