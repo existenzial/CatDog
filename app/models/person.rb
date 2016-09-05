98d806e0836127860a455cbcb324125859879295
Jazz Lyles <ripley36706@gmail.com>
2016-09-05 11:40:43 -0700

    adjusted Person model height to reflect length in inches

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,7 @@
 class Person < ApplicationRecord
   validates :height,
     presence: true,
-    length: { in: 100..230 }, #e.g.: 7 ft = 213 cm
+    length: { in: 36..96 }, #e.g.: 6'0 = 72 in
     numericality: { only_integer: true }
 
   validates :weight,
