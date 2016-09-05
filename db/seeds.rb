0fa255b02381841adea3887023bd339263fef085
Jazz Lyles <ripley36706@gmail.com>
2016-09-04 22:31:52 -0700

    added seed data for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+people = Person.create([
+    { height: 175, weight: 180, prediction: 'dog' },
+    { height: 130, weight: 125, prediction: 'dog' },
+    { height: 155, weight: 157, prediction: 'cat' },
+    { height: 123, weight: 85, prediction: 'dog' },
+    { height: 95, weight: 115, prediction: 'cat' }
+])
